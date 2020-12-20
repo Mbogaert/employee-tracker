aff5b3be9bd17f511385c16a768738355fbac64c
Mathew B <bogaert@email.arizona.edu>
2020-12-19 17:50:19 -0700

    first prompt added

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
+const inquirer = require('inquirer');
 // mysql connections
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
 // connect to database
+// figure this out
+
+function initialPrompt() {
+    return inquirer.prompt(
+        {
+            type: 'list',
+            name: 'initialChoice',
+            message: 'What would you like to do?',
+            choices: ['View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Add a Department',
+                    'Add a Role', 
+                    'Add An Employee',
+                    'Update Employee Role']
+        }
+    )
+}
+
+initialPrompt();
